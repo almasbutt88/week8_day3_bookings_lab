f83522b63f7cf1a680e88d5a562435a8102bbdb9
Almas <m_a_b19@hotmail.com>
2021-12-08 12:02:34 +0000

    first commit

---
+++ server/db/seeds.js
@@ -0,0 +1,22 @@
+use hotel;
+db.dropDatabase();
+
+
+db.bookings.insertMany([
+    {
+        guestName: "John Johns",
+        guestEmail: "MM@jla.com",
+        checkedIn: true
+      },
+      {
+        guestName: "Diana Prince",
+        guestEmail: "WW@jla.com",
+        checkedIn: false
+      },
+      {
+        guestName: "John Stewart",
+        guestEmail: "GL3@jla.com",
+        checkedIn: true
+      }
+    ]);
+
